abd9c89221a6a18eedc457dab1e4894bd21166d1
Singh <2460680@cognizant.com>
2025-12-29 17:45:40 +0530

    added rohin modules

--- MediCart/src/App.jsx
+++ MediCart/src/App.jsx
@@ -5,12 +5,15 @@ import AdminLayout from "./features/admin/AdminLayout";
 import AdminProductsPage from "./features/admin/AdminProductsPage";
 import AdminBatchPage from "./features/admin/AdminBatchPage";
 import AdminLoginPage from "./features/admin/AdminLoginPage";
+import Reports from "./features/admin/analyticsSecction/Reports";
+import Dashboard from "./features/admin/analyticsSecction/dashboard";
 
 export default function App() {
   return (
     <Routes>
       {/* CUSTOMER */}
       <Route path="/" element={<HomePage />} />
+      
 
       {/* ADMIN LOGIN */}
       <Route path="/admin/login" element={<AdminLoginPage />} />
@@ -20,6 +23,8 @@ export default function App() {
         <Route index element={<Navigate to="products" />} />
         <Route path="products" element={<AdminProductsPage />} />
         <Route path="batches" element={<AdminBatchPage />} />
+        <Route path="reports" element={<Reports/>}/>
+        <Route path="dashboard" element={<Dashboard/>}/>
       </Route>
 
       {/* FALLBACK */}
