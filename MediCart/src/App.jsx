1458c5680020f6eaab6872049ea45b85eefdce99
Vedansh15246 <vedansh.agarwal@cognizant.com>
2025-12-29 08:34:04 +0530

    Order Details By Vedansh

--- MediCart/src/App.jsx
+++ MediCart/src/App.jsx
@@ -5,12 +5,18 @@ import AdminLayout from "./features/admin/AdminLayout";
 import AdminProductsPage from "./features/admin/AdminProductsPage";
 import AdminBatchPage from "./features/admin/AdminBatchPage";
 import AdminLoginPage from "./features/admin/AdminLoginPage";
-
+import AddressPage from "./features/delivery/AddressPage";
+import MyOrdersPage from "./order/MyOrdersPage";
+import OrderDetailsPage from "./order/OrderDetailsPage";
 export default function App() {
   return (
     <Routes>
       {/* CUSTOMER */}
       <Route path="/" element={<HomePage />} />
+      <Route path="/address" element={<AddressPage />} /> {/* 👈 new route */}
+
+     <Route path="/orders" element={<MyOrdersPage />} />
+        <Route path="/orders/:orderId" element={<OrderDetailsPage />} />
 
       {/* ADMIN LOGIN */}
       <Route path="/admin/login" element={<AdminLoginPage />} />
